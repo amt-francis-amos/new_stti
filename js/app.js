7bad5cda2c722c84d2aa7808bf2bea9643f1e1ce
francis amos <francis.amos@amalitech.org>
2024-03-08 20:33:25 +0000

    commit to master

--- js/app.js
+++ js/app.js
@@ -41,4 +41,15 @@ $(document).ready(function() {
             }
         }
     });
+
+
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots:true,
+        items:1,
+       
+    })
 });
