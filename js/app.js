72bdb2aebf6316bcdfc282b89250c830a1925bf3
francis amos <francis.amos@amalitech.org>
2024-03-08 02:21:28 +0000

    commit to master

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
         items:1,
         dots:false,
         smartSpeed:1000,
-        navText:
+        navText:[ 'PREV', 'NEXT'],
         responsive:{
             0:{
                
