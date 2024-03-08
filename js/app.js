ce32f44d3f890fcb37a106178e140750c701d2fe
francis amos <francis.amos@amalitech.org>
2024-03-08 19:32:51 +0000

    commit to master

--- js/app.js
+++ js/app.js
@@ -1,58 +1,44 @@
 $(document).ready(function() {
-    // -- HERO SLIDER
+    //--- HERO SLIDER
     $('#hero-slider').owlCarousel({
-        loop:true,
-        margin:0,
-        nav:true,
-        items:1,
-        dots:false,
-        smartSpeed:1000,
-        navText:[ 'PREV', 'NEXT'],
-        responsive:{
-            0:{
-               nav:false,
+        loop: true,
+        margin: 0,
+        nav: true,
+        items: 1,
+        dots: false,
+        smartSpeed: 1000,
+        navText: ['PREV', 'NEXT'],
+        responsive: {
+            0: {
+                nav: false,
             },
-           768:{
-            nav:true,
-           }
+            768: {
+                nav: true,
+            }
         }
     });
 
-
-
-    // -- PROJECT SLIDER
+    // PROJECT SLIDER
     $('#project-slider').owlCarousel({
-        loop:true,
-        margin:24,
-        nav:true,
-        items:1,
-        smartSpeed:1000,
-        navText:[ 'PREV', 'NEXT'],
-        responsive:{
-            0:{
-                items:1,
-                nav:false,
-                margin:0,
+        loop: true,
+        margin: 24,
+        nav: true,
+        items: 1,
+        smartSpeed: 1000,
+        navText: ['PREV', 'NEXT'],
+        responsive: {
+            0: {
+                items: 1,
+                nav: false,
+                margin: 0,
             },
-           768:{
-              items:2,
-           },
-           1140:{
-                items:2,
-                center:true,
-           }
+            768: {
+                items: 2,
+            },
+            1140: {
+                items: 2,
+                center: true,
+            }
         }
-    })
-
-
-
-
-
-
-
-
-})
-
-
-
-
+    });
+});
