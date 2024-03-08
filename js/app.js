2822f72adc07b3d19ea5d0c368ea342c20f44d60
francis amos <francis.amos@amalitech.org>
2024-03-08 00:57:47 +0000

    commit to master

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    // -- HERO SLIDER
+    $('#hero-slider').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        items:1,
+        responsive:{
+            0:{
+               
+            },
+            600:{
+                
+            },
+            1000:{
+                
+            }
+        }
+    })
+})
